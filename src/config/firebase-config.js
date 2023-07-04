29329154713bb96b4f532c0d843881d6df22f64d
Quliyev Aydin <quliyevak@KAPMAC108-M.local>
2023-07-04 21:21:23 +0400

    aura-admin

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
-}
+  apiKey: "AIzaSyB-3jJ90FDFHGxxIdhboITsnk5fMC7-48k",
+  authDomain: "aura-websita.firebaseapp.com",
+  projectId: "aura-websita",
+  storageBucket: "aura-websita.appspot.com",
+  messagingSenderId: "731470548195",
+  appId: "1:731470548195:web:bbb82fce8e7d05730309d6",
+  measurementId: "G-9QCJRXPB8V",
+};
